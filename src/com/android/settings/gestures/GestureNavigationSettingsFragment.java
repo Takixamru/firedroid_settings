7e4f53775998d440ce3c01fc1b41d7e1eb0eb451
jhonboy121 <alfredmathew05@gmail.com>
2021-08-13 15:40:50 +0530

    Settings: allow changing the length of gesture navbar[2/2]
    * forward ported from 11, but incorporated fullscreen gesture settings as a dependency so that this preference will be disabled when pill is hidden.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-Authored-By: David Ramírez <DavidYonatan9@hotmail.com>

--- src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
+++ src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -17,11 +17,13 @@
 package com.android.settings.gestures;
 
 import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.om.IOverlayManager;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.provider.Settings;
@@ -57,6 +59,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
     private static final String RIGHT_EDGE_SEEKBAR_KEY = "gesture_right_back_sensitivity";
 
     private static final String KEY_BACK_HEIGHT = "gesture_back_height";
+    private static final String GESTURE_NAVBAR_LENGTH_KEY = "gesture_navbar_length_preference";
     private static final String IMMERSIVE_NAV_KEY = "immersive_navigation";
 
     private static final String NAV_MODE_IMMERSIVE_OVERLAY = "com.custom.overlay.navbar.gestural";
@@ -70,6 +73,8 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
     private float[] mBackGestureHeightScales = { 0f, 1f, 2f, 3f };
     private int mCurrentRightWidth;
     private int mCurrentLefttWidth;
+
+    private LabeledSeekBarPreference mGestureNavbarLengthPreference;
 
     public GestureNavigationSettingsFragment() {
         super();
@@ -98,6 +103,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
         initSeekBarPreference(LEFT_EDGE_SEEKBAR_KEY);
         initSeekBarPreference(RIGHT_EDGE_SEEKBAR_KEY);
         initSeekBarPreference(KEY_BACK_HEIGHT);
+        initGestureNavbarLengthPreference();
         initImmersiveSwitchPreference();
     }
 
@@ -254,6 +260,18 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
          });
          prefImmersiveNav.setChecked(Settings.Secure.getInt(getContext().getContentResolver(), IMMERSIVE_NAVIGATION_SETTINGS, 0) != 0);
      }
+
+    private void initGestureNavbarLengthPreference() {
+        final ContentResolver resolver = getContext().getContentResolver();
+        mGestureNavbarLengthPreference = getPreferenceScreen().findPreference(GESTURE_NAVBAR_LENGTH_KEY);
+        mGestureNavbarLengthPreference.setContinuousUpdates(true);
+        mGestureNavbarLengthPreference.setProgress(Settings.Secure.getIntForUser(
+            resolver, Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
+            0, UserHandle.USER_CURRENT));
+        mGestureNavbarLengthPreference.setOnPreferenceChangeListener((p, v) ->
+            Settings.Secure.putIntForUser(resolver, Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
+                (Integer) v, UserHandle.USER_CURRENT));
+    }
 
     private static float[] getFloatArray(TypedArray array) {
         int length = array.length();
