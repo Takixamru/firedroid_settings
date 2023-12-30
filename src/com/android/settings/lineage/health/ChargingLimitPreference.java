bezke/packages_apps_Settings_derp

Settings: Add charging control preferences
Change-Id: I936add4df464eb9818258de4955b651266c67ec3
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

src/com/android/settings/derp/health/ChargingLimitPreference.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.derp.health;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.internal.derp.health.HealthInterface;
+
+import com.android.settings.R;
+
+public class ChargingLimitPreference extends Preference
+        implements SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = ChargingLimitPreference.class.getSimpleName();
+
+    private TextView mChargingLimitValue;
+    private SeekBar mChargingLimitBar;
+
+    private final HealthInterface mHealthInterface;
+
+    public ChargingLimitPreference(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        setLayoutResource(R.layout.preference_charging_limit);
+
+        mHealthInterface = HealthInterface.getInstance(context);
+    }
+
+    @Override
+    public void onBindViewHolder(final PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mChargingLimitValue = (TextView) holder.findViewById(R.id.value);
+
+        mChargingLimitBar = (SeekBar) holder.findViewById(R.id.seekbar_widget);
+        mChargingLimitBar.setOnSeekBarChangeListener(this);
+
+        int currLimit = getSetting();
+        mChargingLimitBar.setProgress(currLimit);
+        updateValue(currLimit);
+    }
+
+    @Override
+    public void onStartTrackingTouch(final SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(final SeekBar seekBar) {
+        setSetting(seekBar.getProgress());
+    }
+
+    @Override
+    public void onProgressChanged(final SeekBar seekBar, final int progress,
+            final boolean fromUser) {
+        updateValue(progress);
+    }
+
+    public void setValue(final int value) {
+        if (mChargingLimitBar != null) {
+            mChargingLimitBar.setProgress(value);
+        }
+        updateValue(value);
+    }
+
+    protected int getSetting() {
+        return mHealthInterface.getLimit();
+    }
+
+    protected void setSetting(final int chargingLimit) {
+        mHealthInterface.setLimit(chargingLimit);
+    }
+
+    private void updateValue(final int value) {
+        if (mChargingLimitValue != null) {
+            mChargingLimitValue.setText(String.format("%d%%", value));
+        }
+    }
+}

src/com/android/settings/derp/health/TimePreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.derp.health;
+
+import static java.time.format.FormatStyle.SHORT;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.derp.preference.CustomDialogPreference;
+import com.android.settings.R;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import com.android.internal.derp.health.HealthInterface;
+
+public abstract class TimePreference extends CustomDialogPreference<AlertDialog> {
+    private static final String TAG = TimePreference.class.getSimpleName();
+    private static final DateTimeFormatter mFormatter = DateTimeFormatter.ofLocalizedTime(SHORT);
+
+    private TimePicker mTimePicker;
+    private LocalTime mLocalTime;
+
+    protected HealthInterface mHealthInterface;
+
+    public TimePreference(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        setDialogLayoutResource(R.layout.dialog_time);
+        mHealthInterface = HealthInterface.getInstance(context);
+    }
+
+    @Override
+    public void onBindViewHolder(final PreferenceViewHolder holder) {
+        mLocalTime = LocalTime.ofSecondOfDay(getTimeSetting());
+        super.onBindViewHolder(holder);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(final AlertDialog.Builder builder,
+            final DialogInterface.OnClickListener listener) {
+        super.onPrepareDialogBuilder(builder, listener);
+
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.setPositiveButton(R.string.dlg_ok, null);
+    }
+
+    @Override
+    protected void onDialogClosed(final boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        if (positiveResult) {
+            mLocalTime = LocalTime.of(mTimePicker.getHour(),
+                    mTimePicker.getMinute());
+            setTimeSetting(mLocalTime.toSecondOfDay());
+            setSummary(getSummary());
+        }
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mTimePicker = view.findViewById(R.id.time_picker);
+        mTimePicker.setHour(mLocalTime.getHour());
+        mTimePicker.setMinute(mLocalTime.getMinute());
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return String.format(getContext().getString(getSummaryResourceId()),
+                mLocalTime.format(mFormatter));
+    }
+
+    public void setValue(final int value) {
+        mLocalTime = LocalTime.ofSecondOfDay(value);
+        setSummary(getSummary());
+    }
+
+    protected abstract int getSummaryResourceId();
+
+    protected abstract int getTimeSetting();
+
+    protected abstract void setTimeSetting(int secondOfDay);
+}

Takixamru/firedroid_settings

src/com/android/settings/lineage/health/ChargingLimitPreference.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.lineage.health;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.internal.lineage.health.HealthInterface;
+
+import com.android.settings.R;
+
+public class ChargingLimitPreference extends Preference
+        implements SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = ChargingLimitPreference.class.getSimpleName();
+
+    private TextView mChargingLimitValue;
+    private SeekBar mChargingLimitBar;
+
+    private final HealthInterface mHealthInterface;
+
+    public ChargingLimitPreference(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        setLayoutResource(R.layout.preference_charging_limit);
+
+        mHealthInterface = HealthInterface.getInstance(context);
+    }
+
+    @Override
+    public void onBindViewHolder(final PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mChargingLimitValue = (TextView) holder.findViewById(R.id.value);
+
+        mChargingLimitBar = (SeekBar) holder.findViewById(R.id.seekbar_widget);
+        mChargingLimitBar.setOnSeekBarChangeListener(this);
+
+        int currLimit = getSetting();
+        mChargingLimitBar.setProgress(currLimit);
+        updateValue(currLimit);
+    }
+
+    @Override
+    public void onStartTrackingTouch(final SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(final SeekBar seekBar) {
+        setSetting(seekBar.getProgress());
+    }
+
+    @Override
+    public void onProgressChanged(final SeekBar seekBar, final int progress,
+            final boolean fromUser) {
+        updateValue(progress);
+    }
+
+    public void setValue(final int value) {
+        if (mChargingLimitBar != null) {
+            mChargingLimitBar.setProgress(value);
+        }
+        updateValue(value);
+    }
+
+    protected int getSetting() {
+        return mHealthInterface.getLimit();
+    }
+
+    protected void setSetting(final int chargingLimit) {
+        mHealthInterface.setLimit(chargingLimit);
+    }
+
+    private void updateValue(final int value) {
+        if (mChargingLimitValue != null) {
+            mChargingLimitValue.setText(String.format("%d%%", value));
+        }
+    }
+}

src/com/android/settings/lineage/health/TimePreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.lineage.health;
+
+import static java.time.format.FormatStyle.SHORT;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.lineage.CustomDialogPreference;
+import com.android.settings.R;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import com.android.internal.lineage.health.HealthInterface;
+
+public abstract class TimePreference extends CustomDialogPreference<AlertDialog> {
+    private static final String TAG = TimePreference.class.getSimpleName();
+    private static final DateTimeFormatter mFormatter = DateTimeFormatter.ofLocalizedTime(SHORT);
+
+    private TimePicker mTimePicker;
+    private LocalTime mLocalTime;
+
+    protected HealthInterface mHealthInterface;
+
+    public TimePreference(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        setDialogLayoutResource(R.layout.dialog_time);
+        mHealthInterface = HealthInterface.getInstance(context);
+    }
+
+    @Override
+    public void onBindViewHolder(final PreferenceViewHolder holder) {
+        mLocalTime = LocalTime.ofSecondOfDay(getTimeSetting());
+        super.onBindViewHolder(holder);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(final AlertDialog.Builder builder,
+            final DialogInterface.OnClickListener listener) {
+        super.onPrepareDialogBuilder(builder, listener);
+
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.setPositiveButton(R.string.dlg_ok, null);
+    }
+
+    @Override
+    protected void onDialogClosed(final boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        if (positiveResult) {
+            mLocalTime = LocalTime.of(mTimePicker.getHour(),
+                    mTimePicker.getMinute());
+            setTimeSetting(mLocalTime.toSecondOfDay());
+            setSummary(getSummary());
+        }
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mTimePicker = view.findViewById(R.id.time_picker);
+        mTimePicker.setHour(mLocalTime.getHour());
+        mTimePicker.setMinute(mLocalTime.getMinute());
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return String.format(getContext().getString(getSummaryResourceId()),
+                mLocalTime.format(mFormatter));
+    }
+
+    public void setValue(final int value) {
+        mLocalTime = LocalTime.ofSecondOfDay(value);
+        setSummary(getSummary());
+    }
+
+    protected abstract int getSummaryResourceId();
+
+    protected abstract int getTimeSetting();
+
+    protected abstract void setTimeSetting(int secondOfDay);
+}